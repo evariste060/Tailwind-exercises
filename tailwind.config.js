a4d13e547f4334d310ccebc02891ab15b66b278c
Evariste Nkurunziza <nkurunzizaevariste060@gmail.com>
2025-08-17 14:42:21 +1100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./hello.html","./landing_page.html"
+    ,"./src**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
